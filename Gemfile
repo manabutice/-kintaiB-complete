41d5fbeb047212ee3fc1e9a152fe55f93b5a2cb3
manabutice <manabutice@gmail.com>
2021-03-26 02:52:26 +0000

    Finish japanization-and-time-format

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 
 gem 'rails',        '~> 5.1.6'
+gem 'rails-i18n' # 今回はこのgemを追加しています。
 gem 'bcrypt'
 gem 'faker'
 gem 'bootstrap-sass'
-gem 'will_paginate' # この行を追加してください。
-gem 'bootstrap-will_paginate' # この行を追加してください。
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 gem 'puma',         '~> 3.7'
 gem 'sass-rails',   '~> 5.0'
 gem 'uglifier',     '>= 1.3.0'
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -10,7 +9,9 @@ module AttendanceApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.time_zone = 'Asia/Tokyo' # この１行をここに追加します。
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
